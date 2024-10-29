f6159a3b39f8fc4b3851891df83b1260a3bb3a4e
Eike Mueller <em459@bath.ac.uk>
2024-10-29 08:04:20 +0000

    Renamed file and class

--- src/visualise_lognormal.py
+++ src/visualise_lognormal.py
@@ -1,28 +1,28 @@
 import itertools
 import numpy as np
 from matplotlib import pyplot as plt
-from peds.lognormal_dataset import matern, LogNormalDataset1d
+from peds.lognormal_distribution import matern, LogNormalDistribution1d
 
 n = 256
 Lambda = 0.1  # correlation length
 a_power = 2
 nu = a_power - 1 / 2
 
-dataset = LogNormalDataset1d(n, Lambda, a_power)
+distribution = LogNormalDistribution1d(n, Lambda, a_power)
 
 # Compute covariance estimator
 n_samples = 10000
 cov = np.zeros(n + 1)
 var = 0
 
-for alpha in itertools.islice(iter(dataset), n_samples):
+for alpha in itertools.islice(iter(distribution), n_samples):
     for j in range(n + 1):
         cov[j] += alpha[j] * alpha[3 * n // 4] / n_samples
     var += sum(alpha[:] ** 2) / ((n + 1) * n_samples)
 print(f"var = {var}")
 plt.clf()
 X = np.arange(0, 1 + 0.5 / n, 1 / n)
-for alpha in itertools.islice(iter(dataset), 8):
+for alpha in itertools.islice(iter(distribution), 8):
     plt.plot(X, alpha, linewidth=2)
 ax = plt.gca()
 ax.set_xlabel("x")
