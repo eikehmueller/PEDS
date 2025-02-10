9af821765e3dc950164b9c8e00c04cfdec79eef1
Eike Mueller <eike.h.mueller@gmail.com>
2025-02-10 17:55:57 +0000

    Added redimentary test

--- src/test_diffusion_model2d.py
+++ src/test_diffusion_model2d.py
@@ -1,5 +1,6 @@
 import numpy as np
-from peds.diffusion_model_2d import Solver2d
+import torch
+from peds.diffusion_model_2d import Solver2d, DiffusionModel2d
 
 
 def test_solver2d():
@@ -9,3 +10,13 @@ def test_solver2d():
     alpha = rng.normal(size=(m + 1, m + 1))
     solver = Solver2d(m)
     u = solver.solve(alpha, f_rhs)
+
+
+def test_diffusion_model_2d():
+    rng = np.random.default_rng(seed=215725)
+    batchsize = 4
+    m = 8
+    f_rhs = rng.normal(size=(m, m))
+    alpha = torch.tensor(rng.normal(size=(batchsize, m + 1, m + 1)))
+    model = DiffusionModel2d(f_rhs)
+    u = model(alpha)
