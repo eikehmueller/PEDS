10cdf02fe27104020233fc84e0a71ea159df22f0
Eike Mueller <em459@bath.ac.uk>
2025-02-14 11:40:07 +0000

    Finalised 2d sample generation

--- src/peds/distributions.py
+++ src/peds/distributions.py
@@ -1,7 +1,12 @@
 import numpy as np
 import scipy as sp
+import petsc4py
 
-__all__ = ["matern", "LogNormalDistribution1d"]
+petsc4py.init("-ksp_type preonly -pc_type lu")
+
+from petsc4py import PETSc
+
+__all__ = ["matern", "LogNormalDistribution1d", "LogNormalDistribution2d"]
 
 
 def matern(z, nu):
@@ -16,6 +21,8 @@ def matern(z, nu):
         return np.exp(-np.abs(z))
     elif abs(nu - 3 / 2) < 1e-14:
         return (1 + np.abs(z)) * np.exp(-np.abs(z))
+    elif abs(nu - 1) < 1e-14:
+        return np.abs(z) * sp.special.kv(1, np.abs(z))
     else:
         return (
             0.5 ** (nu - 1)
@@ -28,7 +35,7 @@ def matern(z, nu):
 class LogNormalDistribution1d:
     """Draw samples alpha(x) for the field alpha with
 
-    (-Delta + kappa^2)^a alpha(x) = W(x)
+    (-Delta + kappa^2)^{a/2} alpha(x) = W(x)
 
     on the unit interval where W(x) is white noise. kappa = 1/Lambda is
     the inverse correlation length and homogeneous Neumann BCs are
@@ -82,3 +89,95 @@ class LogNormalDistribution1d:
                 yield sp.linalg.cho_solve_banded((self.L_banded, False), xi)
             else:
                 yield sp.linalg.solve_banded((0, 1), self.L_banded, xi)
+
+
+class LogNormalDistribution2d:
+    """Draw samples alpha(x) for the field alpha with
+
+    (-Delta + kappa^2) alpha(x) = W(x)
+
+    on the unit square where W(x) is white noise. kappa = 1/Lambda is
+    the inverse correlation length and homogeneous Neumann BCs are
+    assumed at all boundaries. The generated field has Matern
+    covariance with nu = 2-d/2 = 1 where d=2 is the dimension.
+    """
+
+    def __init__(self, n, Lambda, seed=141517):
+        """Initialise new instance
+
+        :arg n: number of grid cells
+        :arg Lambda: correlation length
+        :arg seed: seed of random number generator
+        """
+        super().__init__()
+        self.n = n
+        kappa = 1 / Lambda
+        self.rng = np.random.default_rng(seed=seed)
+        h = 1.0 / self.n
+        h_inv_sq = 1 / h**2
+
+        row_ptr = [0]
+        col_indices = []
+        values = []
+        nnz = 0
+        for j in range(self.n + 1):
+            for k in range(self.n + 1):
+                if j > 0:
+                    K_left = h_inv_sq
+                else:
+                    K_left = 0
+                if j < self.n:
+                    K_right = h_inv_sq
+                else:
+                    K_right = 0
+                if k > 0:
+                    K_bottom = h_inv_sq
+                else:
+                    K_bottom = 0
+                if k < self.n:
+                    K_top = h_inv_sq
+                else:
+                    K_top = 0
+                values.append(K_left + K_right + K_bottom + K_top + kappa**2)
+                col_indices.append((self.n + 1) * j + k)
+                nnz += 1
+                if j > 0:
+                    col_indices.append((self.n + 1) * (j - 1) + k)
+                    values.append(-K_left)
+                    nnz += 1
+                if j < self.n:
+                    col_indices.append((self.n + 1) * (j + 1) + k)
+                    values.append(-K_right)
+                    nnz += 1
+                if k > 0:
+                    col_indices.append((self.n + 1) * j + k - 1)
+                    values.append(-K_bottom)
+                    nnz += 1
+                if k < self.n:
+                    col_indices.append((self.n + 1) * j + k + 1)
+                    values.append(-K_top)
+                    nnz += 1
+                row_ptr.append(nnz)
+        # Create sparse matrix, KSP and PETSc solution vector
+        self._petsc_mat = PETSc.Mat().createAIJ((self.n + 1) ** 2, nnz=nnz)
+        self._petsc_mat.setValuesCSR(row_ptr, col_indices, values)
+        self._petsc_mat.assemble()
+        self._ksp = PETSc.KSP().create()
+        self._ksp.setOperators(self._petsc_mat)
+        self._ksp.setFromOptions()
+        self.sigma2 = 1 / (4 * np.pi * kappa**2)
+        self._alpha_vec = PETSc.Vec().createSeq((self.n + 1) ** 2)
+        self._xi_vec = PETSc.Vec().createSeq((self.n + 1) ** 2)
+
+    def __iter__(self):
+        """Iterator over dataset"""
+        while True:
+            with self._xi_vec as v:
+                v[:] = self.rng.normal(
+                    loc=0, scale=self.n / np.sqrt(self.sigma2), size=(self.n + 1) ** 2
+                )
+            self._ksp.solve(self._xi_vec, self._alpha_vec)
+            u = np.empty(shape=(self.n + 1, self.n + 1))
+            with self._alpha_vec as v:
+                u[:, :] = np.asarray(v[:]).reshape((self.n + 1, self.n + 1))
+            yield u
---
+++ src/visualise_lognormal_2d.py
@@ -0,0 +1,67 @@
+import itertools
+from tqdm import tqdm
+import numpy as np
+from matplotlib import pyplot as plt
+from peds.distributions import matern, LogNormalDistribution2d
+
+n = 64
+Lambda = 0.1  # correlation length
+nu = 1
+
+distribution = LogNormalDistribution2d(n, Lambda)
+
+# Compute covariance estimator
+n_samples = 10000
+
+j0, k0 = n // 2, n // 2
+
+d0 = 1e-3
+dmax = d0 + max(
+    np.sqrt(j0**2 + k0**2) / n,
+    np.sqrt((j0 - n) ** 2 + k0**2) / n,
+    np.sqrt(j0**2 + (k0 - n) ** 2) / n,
+    np.sqrt((j0 - n) ** 2 + (k0 - n) ** 2) / n,
+)
+
+nbins = 40
+cov = np.zeros(nbins + 1)
+bin_volume = np.zeros(nbins + 1)
+var = 0
+
+for j in range(n + 1):
+    for k in range(n + 1):
+        d = np.sqrt((j - j0) ** 2 + (k - k0) ** 2) / n
+        bin_idx = int(np.floor(nbins * d / dmax))
+        bin_volume[bin_idx] += 1
+
+for alpha in tqdm(itertools.islice(iter(distribution), n_samples), total=n_samples):
+    for j in range(n + 1):
+        for k in range(n + 1):
+            d = np.sqrt((j - j0) ** 2 + (k - k0) ** 2) / n
+            bin_idx = int(np.floor(nbins * d / dmax))
+            cov[bin_idx + 1] += (
+                alpha[j0, k0] * alpha[j, k] / (n_samples * bin_volume[bin_idx])
+            )
+    cov[0] += alpha[j0, k0] ** 2 / n_samples
+print(f"variance = {cov[0]}")
+plt.clf()
+X = (np.arange(nbins + 1) - 0.5) / nbins * dmax
+X[0] = 0
+plt.plot(
+    X,
+    cov,
+    marker="o",
+    markersize=4,
+    label=r"estimator of $\sigma^{-2}\mathbb{E}[\alpha(x,y)\alpha(x_0,y_0)]$",
+)
+Y = matern((X / Lambda), 1)
+Y[0] = 1
+plt.plot(
+    X,
+    Y,
+    label=r"matern covariance $\sigma^{-2}C_{1,\kappa}(\kappa||x-x_0||)$",
+)
+plt.legend(loc="upper right")
+ax = plt.gca()
+ax.set_xlabel("x")
+plt.savefig("covariance_2d.pdf", bbox_inches="tight")
