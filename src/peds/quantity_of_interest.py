5f27b355fca0df7ac0af29be2a88048a49b1949a
Eike Mueller <em459@bath.ac.uk>
2025-02-20 09:15:14 +0000

    Implement sampling QoI in 2d

--- src/peds/quantity_of_interest.py
+++ src/peds/quantity_of_interest.py
@@ -5,7 +5,7 @@ __all__ = ["QoISampling1d"]
 
 
 class QoISampling1d(torch.nn.Module):
-    """Quantity of interest that evaluates the field at a given set of points"""
+    """Quantity of interest that evaluates the field at a given set of 1d points"""
 
     def __init__(self, sample_points):
         """Initialise new instance
@@ -20,3 +20,21 @@ class QoISampling1d(torch.nn.Module):
         n = x.shape[-1]
         idxs = np.floor(n * self.sample_points)
         return x[..., idxs]
+
+
+class QoISampling2d(torch.nn.Module):
+    """Quantity of interest that evaluates the field at a given set of 2d points"""
+
+    def __init__(self, sample_points):
+        """Initialise new instance
+
+        :arg sample_points: points at which to evaluate the field
+        """
+        super().__init__()
+        self.sample_points = np.asarray(sample_points)
+        assert np.all(0 < self.sample_points) and np.all(self.sample_points < 1)
+
+    def forward(self, x):
+        N = np.asarray(x.shape[-2:])
+        idxs = np.floor(N * self.sample_points).astype(int)
+        return x[..., *idxs.T]
