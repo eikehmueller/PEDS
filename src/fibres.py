49b31ed691ba8ac9459e4f63807c76c732cbd656
Eike Mueller <eike.h.mueller@gmail.com>
2025-04-01 18:20:18 +0100

    Added example code

---
+++ src/fibres.py
@@ -0,0 +1,14 @@
+import itertools
+import numpy as np
+from peds.auxilliary import save_vtk
+from peds.distributions_fibres import FibreDistribution2d, FibreDistributionYang2d
+
+
+n = 256
+n_samples = 8
+distribution = FibreDistributionYang2d(
+    n, volume_fraction=0.55, gaussian_distribution=True
+)
+
+samples = [np.exp(sample) for sample in list(itertools.islice(distribution, n_samples))]
+save_vtk(samples, "fibre_samples.vtk")
