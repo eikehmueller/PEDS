f6e3992cff3e44636a9400424609b9e278998100
Eike Mueller <em459@bath.ac.uk>
2025-04-02 12:16:42 +0100

    Renaming and tidying up

--- src/fibres.py
+++ src/fibres.py
@@ -1,14 +1,12 @@
 import itertools
 import numpy as np
 from peds.auxilliary import save_vtk
-from peds.distributions_fibres import FibreDistribution2d, FibreDistributionYang2d
+from peds.distributions_fibres import FibreDistribution2d
 
 
 n = 256
 n_samples = 8
-distribution = FibreDistributionYang2d(
-    n, volume_fraction=0.55, gaussian_distribution=True
-)
+distribution = FibreDistribution2d(n, volume_fraction=0.55, gaussian_distribution=True)
 
 samples = [np.exp(sample) for sample in list(itertools.islice(distribution, n_samples))]
 save_vtk(samples, "fibre_samples.vtk")
