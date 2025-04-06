4bfaddf2fa94c59ec97c110eaf5a42f2789f02d0
Eike Mueller <eike.h.mueller@gmail.com>
2025-04-06 10:41:59 +0100

    Refactoring of visualisation

--- src/evaluate.py
+++ src/evaluate.py
@@ -20,34 +20,44 @@ from common import (
 )
 
 
-def measure_error(dataset, peds_model, physics_model_highres, scaling_factor):
+def measure_error(
+    dataset, peds_model, physics_model_highres, scaling_factor, sum_qoi_components=False
+):
     """Measure loss values of different models
 
     Meausure the loss function for the PEDS model, the high-fidelity physics model
     and coarsened versions of the physics model.
 
-    Returns a dictonary with the loss values for each model.
+    Returns a dictonary with the error values for each model.
 
     :arg dataset: dataset to use for the loss calculation
     :arg peds_model: PEDS model
     :arg physics_model_highres: high-fidelity physics model
     :arg scaling_factor: scaling factor for the coarsest model
+    :arg sum_qoi_components: if True, sum the components of the error
     """
-    loss_fn = torch.nn.MSELoss()
     sfs = 2 ** (1 + np.arange(int(np.log2(scaling_factor))))
     coarse_model = {sf: get_coarse_model(physics_model_highres, sf, qoi) for sf in sfs}
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=len(dataset))
-    rmse_error = dict()
+    rms_error = dict()
     alpha, q_target = next(iter(dataloader))
     q_pred = peds_model(alpha)
-    rmse_error["peds"] = np.sqrt(loss_fn(q_pred, q_target).detach().item())
-    rmse_error["coarse"] = dict()
+    avg_squared_error = np.average((q_pred - q_target).detach().numpy() ** 2, axis=0)
+    if sum_qoi_components:
+        rms_error["peds"] = np.sqrt(np.sum(avg_squared_error))
+    else:
+        rms_error["peds"] = np.sqrt(avg_squared_error)
+    rms_error["coarse"] = dict()
     for sf, cm in coarse_model.items():
         q_pred_coarse = cm(alpha)
-        rmse_error["coarse"][sf] = np.sqrt(
-            loss_fn(q_pred_coarse, q_target).detach().item()
+        avg_squared_error = np.average(
+            (q_pred_coarse - q_target).detach().numpy() ** 2, axis=0
         )
-    return rmse_error
+        if sum_qoi_components:
+            rms_error["coarse"][sf] = np.sqrt(np.sum(avg_squared_error))
+        else:
+            rms_error["coarse"][sf] = np.sqrt(avg_squared_error)
+    return rms_error
 
 
 def measure_performance(
@@ -98,10 +108,10 @@ def measure_performance(
     return time_per_sample
 
 
-def visualise_performance(rmse_error, time_per_sample, filename):
+def visualise_performance(rms_error, time_per_sample, filename):
     """Plot the error per sample vs. time per sample for different models
 
-    :rmse_error: dictionary with the RMSE error for each model,
+    :rms_error: dictionary with the RMSE error for each model,
         computed by measure_error
     :time_per_sample: dictionary with the time per sample for each model,
         computed by measure_performance
@@ -111,17 +121,17 @@ def visualise_performance(rmse_error, time_per_sample, filename):
 
     plt.plot(
         [time_per_sample["peds"]],
-        [rmse_error["peds"]],
+        [rms_error["peds"]],
         color="red",
         linewidth=2,
         marker="o",
         markersize=6,
         label="PEDS",
     )
-    sfs = sorted(rmse_error["coarse"].keys())
+    sfs = sorted(rms_error["coarse"].keys())
     plt.plot(
         [time_per_sample["coarse"][sf] for sf in sfs],
-        [rmse_error["coarse"][sf] for sf in sfs],
+        [rms_error["coarse"][sf] for sf in sfs],
         linewidth=2,
         color="blue",
         marker="o",
@@ -130,7 +140,7 @@ def visualise_performance(rmse_error, time_per_sample, filename):
     )
     plt.plot(
         [time_per_sample["fine"], time_per_sample["coarse"][2]],
-        [0, rmse_error["coarse"][2]],
+        [0, rms_error["coarse"][2]],
         color="blue",
         linewidth=2,
         linestyle="--",
@@ -150,7 +160,7 @@ def visualise_performance(rmse_error, time_per_sample, filename):
     for sf in sfs:
         plt.annotate(
             f"   ${sf}\\times $",
-            xy=(time_per_sample["coarse"][sf], rmse_error["coarse"][sf]),
+            xy=(time_per_sample["coarse"][sf], rms_error["coarse"][sf]),
         )
 
     ax = plt.gca()
@@ -161,34 +171,23 @@ def visualise_performance(rmse_error, time_per_sample, filename):
     plt.savefig(filename, bbox_inches="tight")
 
 
-def visualise_error(peds_model, coarse_model, dataset, sample_points, filename):
+def visualise_error(rms_error, sample_points, filename):
     """Visualise the RMSE error
 
-    :peds_model: PEDS model
-    :coarse_model: coarse model
+    :rms_error: dictionary with the RMSE error for each model
     :dataset: dataset to use for the error calculation
     :sample_points: Sample points used for for QoI
     :filename: name of file to save the plot to
     """
     sample_points = np.asanyarray(sample_points)
+    scaling_factor = int(np.max(list(rms_error["coarse"].keys())))
     dim = sample_points.ndim
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=len(dataset))
-    alpha, q_target = next(iter(dataloader))
-    q_pred_peds = peds_model(alpha).detach().numpy()
-
-    q_pred_coarse = coarse_model(alpha).detach().numpy()
-    q_target = q_target.detach().numpy()
-    rmse_coarse = np.sqrt(np.mean((q_pred_coarse - q_target) ** 2, axis=0))
-    rmse_peds = np.sqrt(np.mean((q_pred_peds - q_target) ** 2, axis=0))
-
     plt.clf()
     ax = plt.gca()
     if dim == 1:
-        ax.set_xlim(0, 1)
-        ax.set_ylim(0, 1)
         plt.plot(
             sample_points,
-            rmse_peds,
+            rms_error["peds"],
             linewidth=2,
             marker="o",
             markersize=6,
@@ -196,13 +195,15 @@ def visualise_error(peds_model, coarse_model, dataset, sample_points, filename):
         )
         plt.plot(
             sample_points,
-            rmse_coarse,
+            rms_error["coarse"][scaling_factor],
             marker="o",
             markersize=6,
             linewidth=2,
             label="coarse",
         )
         ax.set_yscale("log")
+        ax.set_xlabel("sample point coordinate $x$")
+        ax.set_ylabel("RMS error")
     else:
         _, ax = plt.subplots()
         ax.set_aspect("equal")
@@ -211,7 +212,7 @@ def visualise_error(peds_model, coarse_model, dataset, sample_points, filename):
         ax.scatter(
             sample_points[:, 0],
             sample_points[:, 1],
-            s=2000 * rmse_coarse,
+            s=2000 * rms_error["coarse"][scaling_factor],
             alpha=0.5,
             color="red",
         )
@@ -225,17 +226,17 @@ def visualise_error(peds_model, coarse_model, dataset, sample_points, filename):
         )
         for j in range(sample_points.shape[0]):
             ax.text(
-                sample_points[j, 0] + 0.125 * rmse_coarse[j],
+                sample_points[j, 0] + 0.125 * rms_error["coarse"][scaling_factor][j],
                 sample_points[j, 1],
-                f"{100*rmse_coarse[j]:6.1f}%",
+                f"{100*rms_error["coarse"][scaling_factor][j]:6.1f}%",
                 color="red",
                 verticalalignment="top",
                 size="small",
             )
             ax.text(
-                sample_points[j, 0] + 0.125 * rmse_coarse[j],
+                sample_points[j, 0] + 0.125 * rms_error["coarse"][scaling_factor][j],
                 sample_points[j, 1],
-                f"{100*rmse_peds[j]:6.1f}%",
+                f"{100*rms_error["peds"][j]:6.1f}%",
                 verticalalignment="bottom",
                 color="blue",
                 size="small",
@@ -243,7 +244,7 @@ def visualise_error(peds_model, coarse_model, dataset, sample_points, filename):
         ax.scatter(
             sample_points[:, 0],
             sample_points[:, 1],
-            s=2000 * rmse_peds,
+            s=2000 * rms_error["peds"],
             alpha=0.5,
             color="blue",
         )
@@ -297,21 +298,24 @@ if __name__ == "__main__":
     n_param = sum([torch.numel(p) for p in model.parameters()])
     print(f"number of model parameters = {n_param}")
 
-    rmse_error = measure_error(
+    rms_error = measure_error(
         test_dataset,
         model,
         physics_model_highres,
         scaling_factor,
+        sum_qoi_components=True,
     )
 
     print()
     print("==== error ====")
-    for key, value in rmse_error.items():
+    for key, rmse in rms_error.items():
         if key == "coarse":
-            for scaling_factor, value in value.items():
-                print(f"  rmse error [coarse {scaling_factor:2d}x] = {value:8.4e}")
+            for scaling_factor, coarse_rmse in rmse.items():
+                print(
+                    f"  rmse error [coarse {scaling_factor:2d}x] = {coarse_rmse:8.4e}"
+                )
         else:
-            print(f"  rmse error [{key:10s}] = {value:8.4e}")
+            print(f"  rmse error [{key:10s}] = {rmse:8.4e}")
 
     time_per_sample = measure_performance(
         test_dataset, model, physics_model_highres, scaling_factor, device
@@ -328,9 +332,14 @@ if __name__ == "__main__":
             print(f"  time per sample [{key:10s}] = {1000*value:8.4e} ms")
     print()
 
-    visualise_performance(rmse_error, time_per_sample, "performance.pdf")
+    visualise_performance(rms_error, time_per_sample, "performance.pdf")
 
-    coarse_model = get_coarse_model(physics_model_highres, scaling_factor, qoi)
-    visualise_error(
-        model, coarse_model, test_dataset, config["qoi"]["sample_points"], "rmse.pdf"
+    rms_error = measure_error(
+        test_dataset,
+        model,
+        physics_model_highres,
+        scaling_factor,
+        sum_qoi_components=False,
     )
+
+    visualise_error(rms_error, config["qoi"]["sample_points"], "rmse.pdf")
